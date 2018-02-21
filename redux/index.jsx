e83b862a15cd1927e586fbf20edcd848734165a0
futurewan <393192789@qq.com>
2018-02-21 23:54:53 +0800

    使用react-redux导入store数据源

--- redux/index.jsx
+++ redux/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
-import Provider from "./Provider";
-import store from './Store'
+import { Provider } from "react-redux";
+import store from "./Store";
 import ControlPanel from "./components/ControlPanel.jsx";
 
 ReactDOM.render(
     <Provider store={store}>
         <ControlPanel />
     </Provider>,
-    document.querySelector("#root"));
+    document.querySelector("#root")
+);
